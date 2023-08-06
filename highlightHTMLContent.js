b26182cacfb84c330616a2ce1629c5c011f65143
soyel aktar habib <soyelakterhabib@gmail.com>
2023-08-06 22:39:16 +0530

    Second commit wth edit

--- highlightHTMLContent.js
+++ highlightHTMLContent.js
@@ -1,101 +1,24 @@
-// function highlightHTMLContent(htmlContent, plainText, positions) {
-//   const words = plainText.split(' ');
-//   let editedHtml = htmlContent;
+function highlightHTMLContent(htmlContent, plainText, plainTextPositions){
 
-//   // positions.forEach(pos => {
-//     for(const pos of positions){
-//     // const [startIndex, endIndex] = pos;
-//     const startIndex = pos.start;
-//     const endIndex = pos.end;
+  const tagleft = '<mark>';
+  const tagRight = '</mark>';
+  let diff = 0;
+  for(let i of plainTextPositions){
+    const startPos = i.start;
+    const endPos = i.end;
+    
+    var highlightedContent  = htmlContent.slice(0,startPos+diff)+tagleft+htmlContent.slice(startPos+diff,endPos+diff)+tagRight+htmlContent.slice(endPos+diff+1);// 6 is the size of <mark> and 7 is for </mark>, everytime we put it, we increase it.
 
-//     if (startIndex >= 0 && endIndex >= 0 && endIndex < words.length) {
-//       const editedWord = words[startIndex];
-//       const wordToReplace = words[endIndex];
-//       const startTag = "<mark>";
-//       const endTag = "</mark>";
-//       const replacement = `${startTag}${editedWord}${endTag}`;
-//       editedHtml = editedHtml.replace(new RegExp(`\\b${wordToReplace}\\b`, 'g'), replacement);
-//     }
-//   }
+    diff+=13; // at the end, two mark is added so total size is 13
+    
 
-//   return editedHtml;
-// }
-
-
-
-// function highlightHTMLContent(htmlContent, plainText, plainTextPositions) {
-//   // Iterate through each object in plainTextPositions
-//   for (const position of plainTextPositions) {
-//     const { start, end } = position;
-//     const word = plainText.substring(start, end);
-
-  
-
-//     // Creating a regular expression pattern to match the word in htmlContent
-//     const pattern = new RegExp(`\\b${word}\\b`, 'gi');
-
-//     // Replacing the matched word with the highlighted version using the <mark> tag
-//     htmlContent = htmlContent.replace(pattern, `<mark>${word}</mark>`);
-//   }
-
-//   return htmlContent;
-// }
-
-
-
-
-function highlightHTMLContent(htmlContent, plainText, plainTextPositions) {
-  let highlightedHTML = htmlContent;
-
-  // Helper function to convert plainText position to htmlContent position
-  function getCorrectPosition(start, end) {
-    const startText = plainText.slice(0, start);
-    const startPos = htmlContent.indexOf(startText);
-    const highlightLength = plainText.slice(start, end).length;
-    const endPos = startPos + highlightLength;
-    return { startPos, endPos };
   }
 
-  // Sort the plainTextPositions array in descending order
-  plainTextPositions.sort((a, b) => b.start - a.start);
+  return highlightedContent;
 
-  // Iterate through each position object and highlight the corresponding content
-
-  for (const position of plainTextPositions) {
-    const start = position.start;
-    const end = position.end;
-    const { startPos, endPos } = getCorrectPosition(start, end);
-    var diff=startPos-start;
-    const highlightTag1 = '<mark>';
-    const highlightTag2 = '</mark>';
-    highlightedHTML =
-      highlightedHTML.slice(0, startPos+diff) +
-      highlightTag1 +
-      highlightedHTML.slice(startPos+diff, endPos+diff) +
-      highlightTag2 +
-      highlightedHTML.slice(endPos+diff);
-
-      
-  }
-
-  return highlightedHTML;
 }
 
-// Example usage:
-const htmlContent = '<p><span>Hi David<br>...</span></p>';
-const plainText =
-  'Hi David Headline: Energix Closes $520 Million Financing and Tax Equity...';
-const plainTextPositions = [
-  { start: 8, end: 13 },
-  { start: 24, end: 31 },
-];
 
-const highlightedContent = highlightHTMLContent(
-  htmlContent,
-  plainText,
-  plainTextPositions
-);
-console.log(highlightedContent);
 
 
 
